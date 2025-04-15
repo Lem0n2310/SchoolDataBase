4d8c0df8cbf15880718db61cf34b94c50262c1dc
roma <ulrtatapok@gmail.com>
2025-04-15 22:25:25 +0500

    second

--- main.py
+++ main.py
@@ -65,18 +65,14 @@ def get_table(table):
 
 def add_driver_customer(who, first_name, second_name):
     cursor = connect.cursor()
-    who_id = who + "Id"
     who_table = who + "s"
 
-    cursor.execute(f"""select {who_id} from uskov_klyuchnikov.{who_table} order by {who_id} desc limit 1;""")
     cursor.execute(f"""
-        insert into uskov_klyuchnikov.{who_table} values ((SELECT COALESCE(MIN(t1.id) + 1, 1) 
-     FROM uskov_klyuchnikov.{who_table} t1 
-     WHERE NOT EXISTS (SELECT 1 FROM my_table t2 WHERE t2.id = t1.id + 1)),'{first_name}', '{second_name}');
-        select {who_id} from uskov_klyuchnikov.{who_table} order by {who_id} desc limit 1;
-    """)
+            INSERT INTO uskov_klyuchnikov.{who_table} (firstName, secondName) 
+            VALUES (%s, %s) RETURNING {who}Id;
+        """, (first_name, second_name))
 
-    result = cursor.fetchall()[0][0]
+    result = cursor.fetchone()[0]
     connect.commit()
     cursor.close()
     return result
@@ -97,28 +93,23 @@ def add_order(
     all_drivers = get_id("drivers")
     all_customers = get_id("customers")
 
-    if not (driverId in all_drivers):
-        print("Водителя с таким номером не существует")
-    elif not (customerId in all_customers):
-        print("Пассажира с таким номером не существует")
-    elif not (customerId in all_customers and driverId in all_drivers):
+    if driverId not in all_drivers and customerId not in all_customers:
         print("Ни водителя с таким номером не существует, ни пассажира с таким номером не существует")
+        return "Ни водителя с таким номером не существует, ни пассажира с таким номером не существует"
+    elif driverId not in all_drivers:
+        print("Водителя с таким номером не существует")
+        return "Водителя с таким номером не существует"
+    elif customerId not in all_customers:
+        print("Пассажира с таким номером не существует")
+        return "Пассажира с таким номером не существует"
     else:
-        cursor.execute("""select orderId from uskov_klyuchnikov.taxi order by orderId desc limit 1;""")
-        id = int(cursor.fetchall()[0][0])
         cursor.execute(f"""
-                    insert into uskov_klyuchnikov.taxi values ({id + 1},
-                    '{startDestination}', 
-                    '{endDestination}', 
-                    '{orderTime}', 
-                    '{carNumber}',
-                    '{driverId}',
-                    '{customerId}',
-                    '{status}'
-                    );
-                    select orderId from uskov_klyuchnikov.taxi order by orderId desc limit 1;
-                """)
-        result = cursor.fetchall()[0][0]
+            INSERT INTO uskov_klyuchnikov.taxi (startDestination, endDestination, orderTime, carNumber, driverId, customerId, status) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s) 
+            RETURNING orderId;
+        """, (startDestination, endDestination, orderTime, carNumber, driverId, customerId, status))
+
+        result = cursor.fetchone()[0]
         connect.commit()
         cursor.close()
         return f"Id: {result}"
@@ -129,38 +120,52 @@ def update_order(orderId, status):
 
     all_orders = get_id("taxi")
     if orderId in all_orders:
-        cursor.execute(f"""update uskov_klyuchnikov.taxi set status = '{status}' where orderId = {orderId};""")
-        print("Успешно")
+        cursor.execute("""
+            UPDATE uskov_klyuchnikov.taxi 
+            SET status = %s 
+            WHERE orderId = %s;
+        """, (status, orderId))
+
         connect.commit()
+        print("Успешно")
+        return "Complete!"
     else:
         print("Заказа с таким номером не существует")
+        return "Something went wrong..."
     cursor.close()
 
 
-def delete(table, id):
+def delete_record(table, id):
     cursor = connect.cursor()
     permission = False
+    column = None
+
     if table == "drivers":
         column = "driverId"
-        cursor.execute(f"""select driverId  from  uskov_klyuchnikov.taxi;""")
+        cursor.execute(f"SELECT {column} FROM uskov_klyuchnikov.taxi;")
         result = cursor.fetchall()
         table_ids = [row[0] for row in result]
-        if not (id in table_ids): permission = True
+        if id not in table_ids:
+            permission = True
     elif table == "taxi":
         column = "orderId"
         permission = True
-    else:
+    elif table == "customers":
         column = "customerId"
-        cursor.execute(f"""select customerId  from  uskov_klyuchnikov.taxi;""")
+        cursor.execute(f"SELECT {column} FROM uskov_klyuchnikov.taxi;")
         result = cursor.fetchall()
         table_ids = [row[0] for row in result]
-        if not (id in table_ids): permission = True
+        if id not in table_ids:
+            permission = True
 
     if permission:
-        cursor.execute(f"""delete from uskov_klyuchnikov.{table} where {column} = {id};""")
+        cursor.execute(f"DELETE FROM uskov_klyuchnikov.{table} WHERE {column} = %s;", (id,))
         connect.commit()
-        cursor.close()
         print("Успешно")
+    else:
+        print("Запись с таким идентификатором не существует или удаление запрещено.")
+
+    cursor.close()
 
 
 if __name__ == '__main__':
--- serv.py
+++ serv.py
@@ -3,29 +3,26 @@ from datetime import datetime
 import flask
 import json
 
-from main import get_table, add_driver_customer
+from main import get_table, add_driver_customer, add_order, update_order, delete_record
 
 app = flask.Flask(__name__)
 
 
-class SumRequest:
-    def __init__(self, s1, s2):
-        self.s1 = s1
-        self.s2 = s2
+class UpdateRequest:
+    def __init__(self, order_id, status):
+        self.order_id = order_id
+        self.status = status
 
+
+class DeleteReq:
+    def __init__(self, table, id):
+        self.table = table
+        self.id = id
 
 
 class Request:
     def __init__(self, table_name):
         self.table_name = table_name
-
-
-class SumResponse:
-    def __init__(self, s):
-        self.s = s
-
-    def toJson(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
 
 
 class GetResponse:
@@ -43,6 +40,25 @@ class PostResponseDriverCustomer:
         self.second_name = second_name
 
 
+class PostResponseOrder:
+    def __init__(self,
+                 start_destination,
+                 end_destination,
+                 order_time,
+                 car_number,
+                 driver_id,
+                 customer_id,
+                 status,
+                 ):
+        self.start_destination = start_destination
+        self.end_destination = end_destination
+        self.order_time = order_time
+        self.car_number = car_number
+        self.driver_id = driver_id
+        self.customer_id = customer_id
+        self.status = status
+
+
 class NewResp:
     def __init__(self, s):
         self.s = s
@@ -55,16 +71,6 @@ class NewResp:
         if isinstance(o, datetime):
             return o.isoformat()  # Преобразуем datetime в строку
         return o.__dict__  # Для остальных объектов используем __dict__
-
-
-@app.route("/sum", methods=["POST"])
-def handle():
-    data = flask.request.get_json()
-
-    req = Request(**data)
-    resp = SumResponse(req.s1 + req.s2)
-
-    return resp.toJson(), 200
 
 
 @app.route("/getTable", methods=["GET"])
@@ -91,6 +97,52 @@ def request_add_driver_customer():
     return result.toJson()
 
 
+@app.route("/addOrder", methods=["POST"])
+def request_add_order():
+    data = flask.request.get_json()
+    start_destination = PostResponseOrder(**data).start_destination
+    end_destination = PostResponseOrder(**data).end_destination
+    order_time = PostResponseOrder(**data).order_time
+    car_number = PostResponseOrder(**data).car_number
+    driver_id = PostResponseOrder(**data).driver_id
+    customer_id = PostResponseOrder(**data).customer_id
+    status = PostResponseOrder(**data).status
+
+    result = NewResp(add_order(
+        start_destination,
+        end_destination,
+        order_time,
+        car_number,
+        driver_id,
+        customer_id,
+        status
+    ))
+
+    return result.toJson()
+
+
+@app.route("/updateOrderStatus", methods=["PUT"])
+def request_update_order():
+    data = flask.request.get_json()
+    order_id = UpdateRequest(**data).order_id
+    new_status = UpdateRequest(**data).status
+
+    result = NewResp(update_order(order_id, new_status))
+
+    return result.toJson()
+
+
+@app.route("/delete", methods=["DELETE"])
+def request_delete():
+    data = flask.request.get_json()
+    table = DeleteReq(**data).table
+    id = DeleteReq(**data).id
+
+    result = NewResp(delete_record(table, id))
+
+    return result.toJson()
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=9000)
-    print(request_add_driver_customer)
+    print(request_delete)
